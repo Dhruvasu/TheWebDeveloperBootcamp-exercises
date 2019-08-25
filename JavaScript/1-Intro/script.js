696481897f7b2b17f0951795fe1ee2d403bcd572
Dhruvasu <dhruvasu1@hotmail.com>
2019-08-25 14:20:03 -0400

    add functionality to demonstrate variables and console.log

---
+++ JavaScript/1-Intro/script.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Enter First Name");
+var lastName = prompt("Enter Last Name");
+var age = prompt("Enter age");
+console.log("Your name is " + firstName + " " + lastName);
+console.log("Your age is " + age);
