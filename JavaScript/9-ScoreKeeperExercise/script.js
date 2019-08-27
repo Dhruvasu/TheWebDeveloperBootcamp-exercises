049b84da2e73f425de3530c80c39758e771c504a
Dhruvasu <dhruvasu1@hotmail.com>
2019-08-26 23:59:08 -0400

    add eventListeners for p1 and p2 buttons

--- JavaScript/9-ScoreKeeperExercise/script.js
+++ JavaScript/9-ScoreKeeperExercise/script.js
@@ -12,3 +12,27 @@ var btnreset = document.querySelector("#reset");
 var scores = document.querySelector("h1");
 var p1 = 0;
 var p2 = 0;
+
+btnp1.addEventListener("click", function() {
+	if (p1 < playingTo && p2 < playingTo) {
+		p1 += 1;
+		print();
+	}
+	if (p1 == playingTo) {
+		print();
+		var p1Score = document.querySelector("#p1Score");
+		p1Score.style.color = "red";
+	}
+});
+
+btnp2.addEventListener("click", function() {
+	if (p1 < playingTo && p2 < playingTo) {
+		p2 += 1;
+		print();
+	}
+	if (p2 == playingTo) {
+		print();
+		var p2Score = document.querySelector("#p2Score");
+		p2Score.style.color = "red";
+	}
+});
